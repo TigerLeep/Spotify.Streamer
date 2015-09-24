5d7bc0cd04ac94486ce06d792efedfec49e1bb97
Lee Perkins <leep@tigerbase.com>
2015-09-24 00:22:31 -0400

    Wire up Player UI to Service

--- app/src/main/java/com/tigerbase/spotifystreamer/Player/PlayerFragment.java
+++ app/src/main/java/com/tigerbase/spotifystreamer/Player/PlayerFragment.java
@@ -1,27 +1,35 @@
 package com.tigerbase.spotifystreamer.Player;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v4.app.DialogFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.tigerbase.spotifystreamer.R;
 import com.tigerbase.spotifystreamer.Track;
+import com.tigerbase.spotifystreamer.Player.PlayerService.PlayerBinder;
 
 import java.util.ArrayList;
 
 public class PlayerFragment extends DialogFragment
 {
-    private final static String LOG_TAG = PlayerFragment.class.getName();
+    private final static String LOG_TAG = PlayerFragment.class.getSimpleName();
 
     private TextView _artistName;
     private TextView _albumName;
@@ -36,34 +44,47 @@ public class PlayerFragment extends DialogFragment
 
     private ArrayList<Track> _tracks = null;
     private int _currentTrack = 0;
+    private PlayerService _playerService = null;
+    private Intent _playerServiceIntent = null;
+    private boolean _isPlayerServiceBound = false;
+
+    private ServiceConnection _playerServiceConnection = new ServiceConnection()
+    {
+        private final String LOG_TAG = PlayerFragment.class.getName();
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder binder)
+        {
+            Log.v(LOG_TAG, "onServiceConnected");
+            bindToPlayerService(binder);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name)
+        {
+            Log.v(LOG_TAG, "onServiceDisconnected");
+            _isPlayerServiceBound = false;
+        }
+    };
+
+    @Override
+    public void onStart()
+    {
+        Log.v(LOG_TAG, "onStart");
+        super.onStart();
+        startPlayerService();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         Log.v(LOG_TAG, "onCreateView");
         View view = inflater.inflate(R.layout.fragment_player, null);
-        _artistName = (TextView)view.findViewById(R.id.player_artist_name);
-        _albumName = (TextView)view.findViewById(R.id.player_album_name);
-        _trackName = (TextView)view.findViewById(R.id.player_track_name);
-        _albumThumbnail = (ImageView)view.findViewById(R.id.player_album_thumbnail);
-        _timeSlider = (SeekBar)view.findViewById(R.id.player_time_slider);
-        _skipBackButton = (ImageButton)view.findViewById(R.id.player_skipback_button);
-        _previousButton = (ImageButton)view.findViewById(R.id.player_previous_button);
-        _playButton = (ImageButton)view.findViewById(R.id.player_play_button);
-        _nextButton = (ImageButton)view.findViewById(R.id.player_next_button);
-        _skipForwardButton = (ImageButton)view.findViewById(R.id.player_skipforward_button);
 
-        Bundle bundle;
-        if (savedInstanceState != null)
-        {
-            Log.v(LOG_TAG, "onCreateView: savedInstanceState");
-            bundle = savedInstanceState;
-        }
-        else
-        {
-            Log.v(LOG_TAG, "onCreateView: getArguments");
-            bundle = getArguments();
-        }
+        getViews(view);
+        bindButtonClickListeners();
+
+        Bundle bundle = getIncomingStateBundle(savedInstanceState);
         if (bundle != null)
         {
             Log.v(LOG_TAG, "onCreateView: bundle");
@@ -72,32 +93,6 @@ public class PlayerFragment extends DialogFragment
             displayCurrentTrack();
         }
 
-        _previousButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Log.v(LOG_TAG, "_previousButton.onClick");
-                if (_currentTrack > 0)
-                {
-                    _currentTrack--;
-                }
-                displayCurrentTrack();
-            }
-        });
-        _nextButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Log.v(LOG_TAG, "_nextButton.onClick");
-                if (_currentTrack < _tracks.size() - 1)
-                {
-                    _currentTrack++;
-                }
-                displayCurrentTrack();
-            }
-        });
 
         return view;
     }
@@ -121,14 +116,168 @@ public class PlayerFragment extends DialogFragment
         outState.putInt(getString(R.string.bundle_current_track), _currentTrack);
     }
 
+    @Override
+    public void onDismiss(DialogInterface dialog)
+    {
+        Log.v(LOG_TAG, "onDismiss");
+        super.onDismiss(dialog);
+    }
+
     private void displayCurrentTrack()
     {
+        Log.v(LOG_TAG, "displayCurrentTrack");
         Track track = _tracks.get(_currentTrack);
         _artistName.setText(track.ArtistName); // Why no artist name on phone?
         _albumName.setText(track.AlbumName);
         _trackName.setText(track.Name);
+        if (track.ThumbnailImageUrl != null && !track.ThumbnailImageUrl.isEmpty())
+        {
+            Picasso.with(getActivity())
+                    .load(track.ThumbnailImageUrl)
+                    .into(_albumThumbnail);
+        }
+        else
+        {
+            _albumThumbnail.setImageBitmap(null);
+        }
 
     }
+
+    private void bindToPlayerService(IBinder binder)
+    {
+        Log.v(LOG_TAG, "bindToPlayerService");
+        PlayerBinder playerBinder = (PlayerBinder)binder;
+        _playerService = playerBinder.getService();
+        _playerService.setTracks(_tracks);
+        _playerService.setCurrentTrack(_currentTrack);
+        _playerService.startPlayback();
+        _isPlayerServiceBound = true;
+    }
+
+    private Bundle getIncomingStateBundle(Bundle savedInstanceState)
+    {
+        Log.v(LOG_TAG, "getIncomingStateBundle");
+        Bundle bundle;
+        if (savedInstanceState != null)
+        {
+            Log.v(LOG_TAG, "getIncomingStateBundle: savedInstanceState");
+            bundle = savedInstanceState;
+        }
+        else
+        {
+            Log.v(LOG_TAG, "getIncomingStateBundle: getArguments");
+            bundle = getArguments();
+        }
+        return bundle;
+    }
+
+    private void getViews(View view)
+    {
+        Log.v(LOG_TAG, "getViews");
+        _artistName = (TextView)view.findViewById(R.id.player_artist_name);
+        _albumName = (TextView)view.findViewById(R.id.player_album_name);
+        _trackName = (TextView)view.findViewById(R.id.player_track_name);
+        _albumThumbnail = (ImageView)view.findViewById(R.id.player_album_thumbnail);
+        _timeSlider = (SeekBar)view.findViewById(R.id.player_time_slider);
+        _skipBackButton = (ImageButton)view.findViewById(R.id.player_skipback_button);
+        _previousButton = (ImageButton)view.findViewById(R.id.player_previous_button);
+        _playButton = (ImageButton)view.findViewById(R.id.player_play_button);
+        _nextButton = (ImageButton)view.findViewById(R.id.player_next_button);
+        _skipForwardButton = (ImageButton)view.findViewById(R.id.player_skipforward_button);
+    }
+
+    private void skipBack()
+    {
+        Log.v(LOG_TAG, "skipBack");
+        _playerService.skipBack();
+    }
+
+    private void goToPreviousTrack()
+    {
+        Log.v(LOG_TAG, "goToPreviousTrack");
+        _playerService.previousTrack();
+        _currentTrack = _playerService.getCurrentTrack();
+        displayCurrentTrack();
+    }
+
+    private void startPlayback()
+    {
+        Log.v(LOG_TAG, "startPlayback");
+        displayCurrentTrack();
+        _playerService.setCurrentTrack(_currentTrack);
+        _playerService.startPlayback();
+    }
+
+    private void goToNextTrack()
+    {
+        Log.v(LOG_TAG, "goToNextTrack");
+        _playerService.nextTrack();
+        _currentTrack = _playerService.getCurrentTrack();
+        displayCurrentTrack();
+    }
+
+    private void skipForward()
+    {
+        Log.v(LOG_TAG, "skipForward");
+        _playerService.skipForward();
+    }
+
+    private void startPlayerService()
+    {
+        Log.v(LOG_TAG, "startPlayerService");
+        if(_playerServiceIntent == null)
+        {
+            Context context = getActivity().getApplicationContext();
+            _playerServiceIntent = new Intent(context, PlayerService.class);
+            context.bindService(_playerServiceIntent, _playerServiceConnection, context.BIND_AUTO_CREATE);
+            context.startService(_playerServiceIntent);
+        }
+    }
+
+    private void bindButtonClickListeners()
+    {
+        _skipBackButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                skipBack();
+            }
+        });
+        _previousButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                goToPreviousTrack();
+            }
+        });
+        _playButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startPlayback();
+            }
+        });
+        _nextButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                goToNextTrack();
+            }
+        });
+        _skipForwardButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                skipForward();
+            }
+        });
+    }
+
 
 }
 
--- app/src/main/java/com/tigerbase/spotifystreamer/Player/PlayerService.java
+++ app/src/main/java/com/tigerbase/spotifystreamer/Player/PlayerService.java
@@ -10,6 +10,7 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.wifi.WifiManager;
 import android.net.wifi.WifiManager.WifiLock;
+import android.os.Binder;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.util.Log;
@@ -21,8 +22,10 @@ import com.tigerbase.spotifystreamer.Track;
 import java.io.IOException;
 import java.util.ArrayList;
 
-// The follow sample was used to learn how to implement a service to facilitate audio playback.
+// The follow web pages were used to learn how to implement a service to facilitate audio playback.
 // https://android.googlesource.com/platform/development/+/master/samples/RandomMusicPlayer/
+// http://developer.android.com/guide/topics/media/mediaplayer.html
+// http://code.tutsplus.com/tutorials/create-a-music-player-on-android-song-playback--mobile
 public class PlayerService
         extends Service
         implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
@@ -36,7 +39,8 @@ public class PlayerService
     private static final String ACTION_PREVIOUS_TRACK = "com.tigerbase.spotifystreamer.previous_track";
     private static final String ACTION_NEXT_TRACK = "com.tigerbase.spotifystreamer.next_track";
     private static final String ACTION_STOP = "com.tigerbase.spotifystreamer.stop";
-    private static final String ACTION_REWIND = "com.tigerbase.spotifystreamer.rewind";
+    private static final String ACTION_SKIP_BACK = "com.tigerbase.spotifystreamer.skip_back";
+    private static final String ACTION_SKIP_FORWARD = "com.tigerbase.spotifystreamer.skip_forward";
 
     private static final int NOTIFICATION_ID = 1;
     private static final float DUCK_VOLUME = 0.1f;
@@ -45,10 +49,11 @@ public class PlayerService
     private NotificationManager _notificationManager;
     private Notification _notification;
     private MediaPlayer _mediaPlayer;
+    private IBinder _playerBinder = new PlayerBinder();
 
     private WifiLock _wifiLock;
     private AudioFocus _audioFocus;
-    private ServiceMode _serviceMode;
+    private ServiceMode _serviceMode = ServiceMode.Stopped;
 
     private ArrayList<Track> _tracks;
     private int _currentTrack;
@@ -72,7 +77,14 @@ public class PlayerService
     @Override
     public int onStartCommand(Intent intent, int flags, int startId)
     {
+        Log.v(LOG_TAG, "onStartCommand");
         String action = intent.getAction();
+        if (action == null)
+        {
+            Log.v(LOG_TAG, "onStartCommand: action == null");
+            return START_NOT_STICKY;
+        }
+
         switch (action)
         {
             case ACTION_PLAY:
@@ -90,8 +102,11 @@ public class PlayerService
             case ACTION_STOP:
                 stopPlayback();
                 break;
-            case ACTION_REWIND:
-                rewindPlayback();
+            case ACTION_SKIP_BACK:
+                skipBack();
+                break;
+            case ACTION_SKIP_FORWARD:
+                skipForward();
                 break;
         }
         return START_NOT_STICKY;
@@ -100,6 +115,7 @@ public class PlayerService
     @Override
     public void onAudioFocusChange(int focusChange)
     {
+        Log.v(LOG_TAG, "onAudioFocusChange");
         switch (focusChange)
         {
             case AudioManager.AUDIOFOCUS_LOSS:
@@ -118,18 +134,21 @@ public class PlayerService
     @Override
     public void onCompletion(MediaPlayer mp)
     {
-
+        Log.v(LOG_TAG, "onCompletion");
+        releaseAllResources();
     }
 
     @Override
     public boolean onError(MediaPlayer mp, int what, int extra)
     {
+        Log.v(LOG_TAG, "onError");
         return false;
     }
 
     @Override
     public void onPrepared(MediaPlayer mp)
     {
+        Log.v(LOG_TAG, "onPrepared");
         _serviceMode = ServiceMode.Playing;
         updateExistingNotification(_tracks.get(_currentTrack).Name + " (Playing)");
         configureAndStartMediaPlayer();
@@ -138,41 +157,52 @@ public class PlayerService
     @Override
     public IBinder onBind(Intent intent)
     {
-        return null;
+        Log.v(LOG_TAG, "onBind");
+        return _playerBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent)
+    {
+        Log.v(LOG_TAG, "onUnbind");
+        super.onUnbind(intent);
+        releaseAllResources();
+
+        return false;
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        Log.v(LOG_TAG, "onDestroy");
+        releaseAllResources();
     }
 
     public void setTracks(ArrayList<Track> tracks)
     {
+        Log.v(LOG_TAG, "setTracks");
         _tracks = tracks;
     }
 
     public ArrayList<Track> getTracks()
     {
+        Log.v(LOG_TAG, "getTracks");
         return _tracks;
     }
 
-    public void setCurrentTrack(String trackId)
-    {
-        for (int index = 0; index < _tracks.size(); index++)
-        {
-            if(_tracks.get(index).Id == trackId)
-            {
-                _currentTrack = index;
-            }
-        }
-    }
-
-
     private void initializeWifiLock()
     {
+        Log.v(LOG_TAG, "initializeWifiLock");
         WifiManager wifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
         _wifiLock = wifiManager.createWifiLock(WifiManager.WIFI_MODE_FULL, WIFI_LOCK_TAG);
     }
 
     private void initializeMediaPlayerIfNeeded()
     {
+        Log.v(LOG_TAG, "initializeMediaPlayerIfNeeded");
         if (_mediaPlayer == null)
         {
+            Log.v(LOG_TAG, "initializeMediaPlayerIfNeeded: mediaPlayer == null");
             _mediaPlayer = new MediaPlayer();
 
             _mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
@@ -184,12 +214,14 @@ public class PlayerService
         }
         else
         {
+            Log.v(LOG_TAG, "initializeMediaPlayerIfNeeded: mediaPlayer != null");
             _mediaPlayer.reset();
         }
     }
 
     private void requestAudioFocus()
     {
+        Log.v(LOG_TAG, "requestAudioFocus");
         if (_audioFocus != AudioFocus.Focused)
         {
             int status = _audioManager.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
@@ -202,6 +234,7 @@ public class PlayerService
 
     private void releaseAudioFocus()
     {
+        Log.v(LOG_TAG, "releaseAudioFocus");
         if (_audioFocus == AudioFocus.Focused)
         {
             int status = _audioManager.abandonAudioFocus(this);
@@ -212,42 +245,47 @@ public class PlayerService
         }
     }
 
-    private void startPlayback()
+    public void startPlayback()
     {
-        if (_serviceMode == ServiceMode.Paused)
+        Log.v(LOG_TAG, "startPlayback");
+        switch (_serviceMode)
         {
-            requestAudioFocus();
-            _serviceMode = ServiceMode.Playing;
-            bringServiceToForeground(getPlayingNotificationMessage());
-            configureAndStartMediaPlayer();
-        }
-        else if (_serviceMode == ServiceMode.Stopped)
-        {
-            requestAudioFocus();
-            releaseAllResourcesExceptMediaPlayer();
-            try
-            {
-                initializeMediaPlayerIfNeeded();
-                Track track = getCurrentTrack();
-                if(track != null)
+            case Paused:
+                requestAudioFocus();
+                _serviceMode = ServiceMode.Playing;
+                bringServiceToForeground(getPlayingNotificationMessage());
+                configureAndStartMediaPlayer();
+                break;
+            case Stopped:
+            case Playing:
+                stopPlayback();
+                requestAudioFocus();
+                releaseAllResourcesExceptMediaPlayer();
+                try
                 {
-                    _mediaPlayer.setDataSource(track.PreviewUrl);
+                    initializeMediaPlayerIfNeeded();
+                    Track track = _tracks.get(getCurrentTrack());
+                    if(track != null)
+                    {
+                        _mediaPlayer.setDataSource(track.PreviewUrl);
+                    }
+                    _serviceMode = ServiceMode.Buffering;
+                    bringServiceToForeground(getBufferingNotificationMessage());
+                    _mediaPlayer.prepareAsync();
+                    _wifiLock.acquire();
                 }
-                _serviceMode = ServiceMode.Buffering;
-                bringServiceToForeground(getBufferingNotificationMessage());
-                _mediaPlayer.prepareAsync();
-                _wifiLock.acquire();
-            }
-            catch (IOException ex)
-            {
-                Log.e(LOG_TAG, ex.getMessage());
-                ex.printStackTrace();
-            }
+                catch (IOException ex)
+                {
+                    Log.e(LOG_TAG, ex.getMessage());
+                    ex.printStackTrace();
+                }
+                break;
         }
     }
 
-    private void pausePlayback()
+    public void pausePlayback()
     {
+        Log.v(LOG_TAG, "pausePlayback");
         if (_serviceMode == ServiceMode.Playing)
         {
             _serviceMode = ServiceMode.Paused;
@@ -256,48 +294,107 @@ public class PlayerService
         }
     }
 
-    private void duckPlayback()
+    public void duckPlayback()
     {
-
+        Log.v(LOG_TAG, "duckPlayback");
+        setVolumeToLow();
     }
 
-    private void stopPlayback()
+    public void stopPlayback()
     {
+        Log.v(LOG_TAG, "stopPlayback");
         if(_serviceMode == ServiceMode.Playing || _serviceMode == ServiceMode.Paused)
         {
-            _serviceMode = ServiceMode.Stopped;
             releaseAllResources();
             releaseAudioFocus();
 
         }
     }
 
-    private void previousTrack()
+    public void previousTrack()
     {
-
+        Log.v(LOG_TAG, "previousTrack");
+        if(_serviceMode == ServiceMode.Buffering)
+        {
+            return;
+        }
+        if (_currentTrack > 0)
+        {
+            _currentTrack--;
+        }
+        startPlayback();
     }
 
-    private void nextTrack()
+    public void nextTrack()
     {
-
+        Log.v(LOG_TAG, "nextTrack");
+        if(_serviceMode == ServiceMode.Buffering)
+        {
+            return;
+        }
+        if (_currentTrack < _tracks.size() - 1)
+        {
+            _currentTrack++;
+        }
+        startPlayback();
     }
 
-    private void rewindPlayback()
+    public void skipBack()
     {
+        Log.v(LOG_TAG, "skipBack");
+        if(_serviceMode == ServiceMode.Buffering)
+        {
+            return;
+        }
+        if (_mediaPlayer != null)
+        {
+            int position = _mediaPlayer.getCurrentPosition();
+            position -= 5000;
+            if (position < 0)
+            {
+                position = 0;
+            }
+            _mediaPlayer.seekTo(position);
+        }
+    }
 
+    public void skipForward()
+    {
+        Log.v(LOG_TAG, "skipForward");
+        if(_serviceMode == ServiceMode.Buffering)
+        {
+            return;
+        }
+        if (_mediaPlayer != null)
+        {
+            int position = _mediaPlayer.getCurrentPosition();
+            int duration = _mediaPlayer.getDuration();
+            position += 5000;
+            if (position > duration)
+            {
+                position = duration - 1000;
+            }
+            if (position < 0)
+            {
+                position = 0;
+            }
+            _mediaPlayer.seekTo(position);
+        }
     }
 
     private void releaseAllResources()
     {
+        Log.v(LOG_TAG, "releaseAllResources");
         releaseAllResourcesExceptMediaPlayer();
         releaseMediaPlayer();
     }
 
     private void releaseAllResourcesExceptMediaPlayer()
     {
+        Log.v(LOG_TAG, "releaseAllResourcesExceptMediaPlayer");
         stopForeground(true);
 
-        if (_wifiLock.isHeld())
+        if (_wifiLock != null && _wifiLock.isHeld())
         {
             _wifiLock.release();
         }
@@ -305,13 +402,19 @@ public class PlayerService
 
     private void releaseMediaPlayer()
     {
-        _mediaPlayer.reset();
-        _mediaPlayer.release();
-        _mediaPlayer = null;
+        Log.v(LOG_TAG, "releaseMediaPlayer");
+        _serviceMode = ServiceMode.Stopped;
+        if (_mediaPlayer != null)
+        {
+            _mediaPlayer.reset();
+            _mediaPlayer.release();
+            _mediaPlayer = null;
+        }
     }
 
     private void updateExistingNotification(String notificationText)
     {
+        Log.v(LOG_TAG, "updateExistingNotification");
         Intent intent = new Intent(getApplicationContext(), ArtistSearchActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         _notification.setLatestEventInfo(getApplicationContext(), "Spotify Streamer", notificationText, pendingIntent);
@@ -320,6 +423,7 @@ public class PlayerService
 
     private void createNewNotification(String notificationText)
     {
+        Log.v(LOG_TAG, "createNewNotification");
         Intent intent = new Intent(getApplicationContext(), ArtistSearchActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         _notification = new Notification();
@@ -331,6 +435,7 @@ public class PlayerService
 
     private void configureAndStartMediaPlayer()
     {
+        Log.v(LOG_TAG, "configureAndStartMediaPlayer");
         if (_audioFocus == AudioFocus.NotFocused)
         {
             pauseMediaPlayerIfPlaying();
@@ -348,6 +453,7 @@ public class PlayerService
 
     private void startMediaPlayerIfNotPlaying()
     {
+        Log.v(LOG_TAG, "startMediaPlayerIfNotPlaying");
         if (!_mediaPlayer.isPlaying())
         {
             _mediaPlayer.start();
@@ -356,6 +462,7 @@ public class PlayerService
 
     private void pauseMediaPlayerIfPlaying()
     {
+        Log.v(LOG_TAG, "pauseMediaPlayerIfPlaying");
         if (_mediaPlayer.isPlaying())
         {
             _mediaPlayer.pause();
@@ -364,35 +471,39 @@ public class PlayerService
 
     private void setVolumeToLow()
     {
-        //
+        Log.v(LOG_TAG, "setVolumeToLow");
         _mediaPlayer.setVolume(DUCK_VOLUME, DUCK_VOLUME);
     }
 
     private void setVolumeToFull()
     {
-        //
+        Log.v(LOG_TAG, "setVolumeToFull");
         _mediaPlayer.setVolume(1.0f, 1.0f);
     }
 
     private void bringServiceToForeground(String notificationText)
     {
+        Log.v(LOG_TAG, "bringServiceToForeground");
         createNewNotification(notificationText);
         startForeground(NOTIFICATION_ID, _notification);
     }
 
     private String getPlayingNotificationMessage()
     {
+        Log.v(LOG_TAG, "getPlayingNotificationMessage");
         return getNotificationMessage(getString(R.string.notification_playing_message));
     }
 
     private String getBufferingNotificationMessage()
     {
+        Log.v(LOG_TAG, "getBufferingNotificationMessage");
         return getNotificationMessage(getString(R.string.notification_buffering_message));
     }
 
     private String getNotificationMessage(String notificationFormatText)
     {
-        Track track = getCurrentTrack();
+        Log.v(LOG_TAG, "getNotificationMessage");
+        Track track = _tracks.get(getCurrentTrack());
         if(track == null)
         {
             return "";
@@ -402,18 +513,31 @@ public class PlayerService
 
     private boolean isCurrentTrackValid()
     {
+        Log.v(LOG_TAG, "isCurrentTrackValid");
         return !(_tracks == null || _currentTrack < 0 || _currentTrack >= _tracks.size());
     }
 
-    private Track getCurrentTrack()
+    public int getCurrentTrack()
     {
-        if (!isCurrentTrackValid())
-        {
-            return null;
-        }
-        return _tracks.get(_currentTrack);
+        Log.v(LOG_TAG, "getCurrentTrack");
+        return _currentTrack;
     }
 
+    public void setCurrentTrack(int currentTrack)
+    {
+        Log.v(LOG_TAG, "setCurrentTrack");
+        _currentTrack = currentTrack;
+    }
 
+    public class PlayerBinder extends Binder
+    {
+        private final String LOG_TAG = PlayerBinder.class.getSimpleName();
+
+        PlayerService getService()
+        {
+            Log.v(LOG_TAG, "getService");
+            return PlayerService.this;
+        }
+    }
 
 }
